533ed7ec20bd462dedc6247da7a2a6ebb63b5be3
Riccardo Di Curti <info@riccardodicurti.it>
2022-09-05 13:31:49 +0200

    0.0.1

---
+++ disable-update-notification-emails.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Plugin Name: Disable update notification emails
+ * Plugin URI: https://github.com/riccardodicurti/disable-update-notification-emails
+ * GitHub Plugin URI: riccardodicurti/disable-update-notification-emails
+ * Primary Branch: main
+ * Description: Disable all update notification emails from core, plugin, themes.
+ * Author: Riccardo Di Curti
+ * Version: 0.0.1
+ * Author URI: https://riccardodicurti.it/
+ **/
+
+// Disable core update emails
+add_filter( 'auto_core_update_send_email', '__return_false' );
+
+// Disable plugin update emails
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+
+// Disable theme update emails
+add_filter( 'auto_theme_update_send_email', '__return_false' );
